6aa857c26b877f4f9e847d254d81ab391300608b
matheusDubberstein <matheusdub.s@gmail.com>
2020-04-22 13:33:19 -0300

    add: profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,108 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Img } from './styles';
+import angular from '~/assets/images/tech/angularjs.svg';
+import css from '~/assets/images/tech/css.svg';
+import html from '~/assets/images/tech/html.svg';
+import javascript from '~/assets/images/tech/js.svg';
+import mongo from '~/assets/images/tech/mongo.svg';
+import nodejs from '~/assets/images/tech/nodejs.svg';
+import postgres from '~/assets/images/tech/postgres.svg';
+import reactRedux from '~/assets/images/tech/react-redux.svg';
+import react from '~/assets/images/tech/react.svg';
+import vue from '~/assets/images/tech/vue.svg';
+import profile from '~/assets/Profile.jpeg';
+import instagram from '~/assets/images/social/instagram.svg';
+import linkedin from '~/assets/images/social/linkedin.svg';
+import github from '~/assets/images/social/github.svg';
 
 export default function Login() {
-  return <Container>Login</Container>;
+  return (
+    <Container>
+      <div>
+        <header>
+          <img src={profile} alt="Matheus Dubberstein da Silva" />
+          <div>
+            <strong>Matheus Dubberstein da Silva</strong>
+            <small>desenvolvedor web / Designer</small>
+          </div>
+        </header>
+        <aside>
+          <a href="https://www.github.com/matheusDubberstein" target="_blank">
+            <img src={github} alt="github.com/matheusDubberstein" />
+            <small>Matheus Dubberstein</small>
+          </a>
+          <a
+            href="https://www.instagram.com/matheus_dubberstein/"
+            target="_blank"
+          >
+            <img src={instagram} alt="instagram/matheus_dubberstein" />
+            <small>@matheus_dubberstein</small>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/matheus-dubberstein-39a9bb154/"
+            target="_blank"
+          >
+            <img
+              src={linkedin}
+              alt="https://www.linkedin.com/in/matheus-dubberstein-39a9bb154/"
+            />
+            <small>Matheus Dubberstein</small>
+          </a>
+        </aside>
+      </div>
+      <footer>
+        <header>
+          <strong>Sobre</strong>
+          <small>
+            Trabalho no setor de desenvolvimento de software desde 2016, já pude
+            experimentar diversas tecnologias como Java, C, PostgreSQL, MongoDB,
+            Javascript junto com seus Framework(React, React Native, ReactJS,
+            NodeJS, AngularJS, VueJS). Passar por essas experiencias foram muito
+            boas pra ter um rumo determinado, hoje sou desenvolvedor Full Stack
+            usando (ReactJS, React Native, NodeJs, PostgreSQL) sendo que tenho
+            mais carinho com a parte do Front-end, tenho experiencia com
+            controle de versões usando Git.
+          </small>
+        </header>
+        <aside>
+          <strong>Tecnologias</strong>
+          <div>
+            <Img>
+              <img src={html} alt="HTML5" />
+            </Img>
+            <Img>
+              <img src={css} alt="CSS5" />
+            </Img>
+            <Img>
+              <img src={postgres} alt="PostgresSQL" />
+            </Img>
+            <Img isMongo>
+              <img src={mongo} alt="MongoDB" />
+            </Img>
+          </div>
+          <div>
+            <Img>
+              <img src={javascript} alt="Javascript" />
+            </Img>
+            <Img>
+              <img src={nodejs} alt="NodeJS" />
+            </Img>
+            <Img>
+              <img src={vue} alt="VueJS" />
+            </Img>
+            <Img>
+              <img src={angular} alt="AngularJS" />
+            </Img>
+            <Img>
+              <img src={react} alt="ReactJs" />
+            </Img>
+            <Img>
+              <img src={reactRedux} alt="React Redux" />
+            </Img>
+          </div>
+        </aside>
+      </footer>
+    </Container>
+  );
 }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,3 +1,171 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 25px;
+  > div {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+    > header {
+      > img {
+        height: 300px;
+        width: 300px;
+        border-radius: 50%;
+        border: 10px solid #f9fafd;
+      }
+      > div {
+        display: flex;
+        flex-direction: column;
+        > strong {
+          font-size: 40px;
+          color: #e2e4eb;
+        }
+        > small {
+          font-size: 20px;
+          color: #cccfd9;
+        }
+      }
+    }
+    > aside {
+      display: flex;
+      flex-direction: column;
+      > a {
+        margin: 5px 0;
+        display: flex;
+        align-items: flex-end;
+        > img {
+          height: 25px;
+          width: 25px;
+          margin: 0 5px 0 0;
+        }
+        > small {
+          font-size: 14px;
+          color: #e2e4eb;
+          transition: color 0.2s;
+          :hover {
+            color: ${darken(0.2, '#E2E4EB')};
+          }
+        }
+      }
+    }
+  }
+  > footer {
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: 60% 40%;
+    > header {
+      border-right: 2px solid #858691;
+      display: flex;
+      flex-direction: column;
+      > strong {
+        font-size: 40px;
+        color: #e2e4eb;
+      }
+      > small {
+        font-size: 20px;
+        color: #cccfd9;
+      }
+    }
+    > aside {
+      display: flex;
+      flex-direction: column;
+      > strong {
+        font-size: 40px;
+        color: #e2e4eb;
+      }
+    }
+  }
+  @media (max-width: 600px) {
+    padding: 10px;
+    > div {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 20px;
+      > header {
+        > img {
+          height: 150px;
+          width: 150px;
+          border-radius: 50%;
+          border: 5px solid #f9fafd;
+        }
+        > div {
+          display: flex;
+          flex-direction: column;
+          > strong {
+            font-size: 15px;
+            color: #e2e4eb;
+          }
+          > small {
+            font-size: 10px;
+            color: #cccfd9;
+          }
+        }
+      }
+      > aside {
+        display: flex;
+        flex-direction: column;
+        > a {
+          margin: 5px 0;
+          display: flex;
+          align-items: flex-end;
+          > img {
+            height: 15px;
+            width: 15px;
+            margin: 0 5px 0 0;
+          }
+          > small {
+            font-size: 14px;
+            color: #e2e4eb;
+            transition: color 0.2s;
+            :hover {
+              color: ${darken(0.2, '#E2E4EB')};
+            }
+          }
+        }
+      }
+    }
+    > footer {
+      display: grid;
+      grid-gap: 10px;
+      grid-template-columns: 50% 50%;
+      > header {
+        border-right: 2px solid #858691;
+        display: flex;
+        flex-direction: column;
+        > strong {
+          font-size: 15px;
+          color: #e2e4eb;
+        }
+        > small {
+          font-size: 10px;
+          color: #cccfd9;
+        }
+      }
+      > aside {
+        display: flex;
+        flex-direction: column;
+        > strong {
+          font-size: 20px;
+          color: #e2e4eb;
+        }
+      }
+    }
+  }
+`;
+
+export const Img = styled.a`
+  margin: 0 10px;
+  @media (max-width: 600px) {
+    margin: 0 2.5px;
+  }
+
+  > img {
+    height: 100px;
+    width: ${(props) => (props.isMongo ? '320px' : '100px')};
+    @media (max-width: 600px) {
+      height: 25px;
+      width: ${(props) => (props.isMongo ? '77px' : '25px')};
+    }
+  }
+`;
